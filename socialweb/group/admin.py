35de6a2ba391cf6efe050f04aa7002ca4f4a8d25
Lakshay <lb2494@gmail.com>
2019-01-29 17:46:14 +0530

    groups app templates created

--- socialweb/group/admin.py
+++ socialweb/group/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class GroupMembersInline(admin.TabularInline):
+    model = models.GroupMember
+
+
+admin.register(models.Group)
